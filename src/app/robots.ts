3bd49f7d6cba14310bb16d872f60449579b8d03a
Rohit Singh <rohit.singh.33521@gmail.com>
2026-03-04 15:52:12 +0530

    update bots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,11 +4,17 @@ import { getSiteUrl } from "@/lib/seo/metadata";
 export default function robots(): MetadataRoute.Robots {
   const siteUrl = getSiteUrl();
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: ["/api/"],
-    },
+    rules: [
+      {
+        userAgent: ["GPTBot", "ChatGPT-User", "CCBot", "MetaBot" , "AhrefsSiteAudit"],
+        disallow: "/",
+      },
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/api/"],
+      },
+    ],
     sitemap: `${siteUrl}/sitemap.xml`,
     host: siteUrl,
   };
