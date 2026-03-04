13deb850c12a1affcb8480420b90aee447c8d98d
Rohit Singh <rohit.singh.33521@gmail.com>
2026-03-04 15:54:14 +0530

    update bots block list

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
-        userAgent: ["GPTBot", "ChatGPT-User", "CCBot", "MetaBot" , "AhrefsSiteAudit"],
+        userAgent: ["GPTBot", "ChatGPT-User", "CCBot", "MetaBot" , "AhrefsSiteAudit" , "Bytespider" , "Meta-ExternalAgent" , "AhrefsBot" , ],
         disallow: "/",
       },
       {
