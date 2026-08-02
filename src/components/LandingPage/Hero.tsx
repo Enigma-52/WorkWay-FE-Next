7d6652048860d6601645972bcf7fd6ce9e947cfa
Rohit Singh <rohit.singh.33521@gmail.com>
2026-08-02 12:50:21 +0530

    landing: bump hero job count to 450k+

--- src/components/LandingPage/Hero.tsx
+++ src/components/LandingPage/Hero.tsx
@@ -70,7 +70,7 @@ const Hero = () => {
           style={{ animationDelay: "550ms" }}
         >
           {[
-            { n: "400k+", l: "Jobs" },
+            { n: "450k+", l: "Jobs" },
             { n: "5k+", l: "Companies" },
             { n: "50+", l: "Domains" },
           ].map((s) => (
